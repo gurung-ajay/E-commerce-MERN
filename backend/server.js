fb3f3d20bd145fce46b8d95080f22bd97b23aa63
Ajay Gurung <gooronz.ajay@gmail.com>
2025-02-24 12:07:47 +0545

    added delete and get methods

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,18 @@ const app = express();
 
 app.use(express.json()); // allows us to accept to accept JSON data in the req.body
 
+
+app.get("/api/products", async (req, res) => {
+    try {
+        const products = await Product.find({}); // if you pass empty obj in find, it fetches all the data in the database
+        res.status(200).json({ success: true, data: products })
+    } catch (error) {
+        console.error("Error in fetching products: ", error.message);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
+})
+
+
 app.post("/api/products", async (req, res) => {
     const product = req.body; // user will send this data
 
@@ -28,7 +40,22 @@ app.post("/api/products", async (req, res) => {
     }
 });
 
-console.log(process.env.MONGO_URI);
+
+app.delete("/api/products/:id", async (req, res) => {
+    const {id} = req.params;
+
+    try {
+        await Product.findByIdAndDelete(id)
+        res.status(200).json({ success: true, message: "Product deleted" })
+    } catch (error) {
+        console.error("Error message: ", error.message)
+        res.status(404).json({ success:"false", message: "Product not found" })
+    }
+});
+
+
+
+
 
 app.listen(5000, () => {
     connectDB();
